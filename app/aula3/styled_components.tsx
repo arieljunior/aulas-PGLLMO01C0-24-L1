4828beb5aa7f78267a1429ca1e58802747204ac1
unknown <arieljuniorc@outlook.com>
2025-10-20 20:11:23 -0300

    fix: estruta de pastas e arquivos

--- app/aula3/styled_components.tsx
+++ app/aula3/styled_components.tsx
@@ -1,6 +1,6 @@
+import { ButtomCustom as Button } from '@/components/Button';
+import Card from '@/components/Card';
 import { View } from "react-native";
-import { ButtomCustom as Button } from '../components/Button';
-import Card from '../components/Card';
 
 export default function Index() {
     return (
